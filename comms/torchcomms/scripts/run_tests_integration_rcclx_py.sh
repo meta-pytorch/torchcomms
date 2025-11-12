923233fcedcba68274592fec88e1eb1c00262c2d
Sudharssun Subramanian <sudharssun@meta.com>
2025-11-11 17:11:39 -0800

    Integrate RCCLX in setup.py and tweaks to Cmakelists
    Summary:
    This adds integration to setup.py for RCCLX backend, and some tweaks to CMakeLists to take USE_RCCLX parameter to build the backend and install it.
    
    Also add the run_integration_tests script for rcclx backend and a setup_rcclx.sh. Once D84125652 is in, I will have a separate build_test.yaml for RCCLX on mi350_runner.
    
    Reviewed By: siyengar
    
    Differential Revision: D86221377
    
    fbshipit-source-id: 04a9f8d14debbad67cf05041433306956b8661ce

---
+++ comms/torchcomms/scripts/run_tests_integration_rcclx_py.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# DO NOT DELETE
+# This script runs the Python integration tests.
+# This is used as part of the GitHub CI.
+
+set -ex
+
+cd "$(dirname "$0")/../tests/integration/py"
+
+run_tests () {
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 AllGatherTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 AllGatherSingleTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 AllReduceTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 BarrierTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 BroadcastTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 BatchSendRecvTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 ReduceScatterTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 ReduceTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 SendRecvTest.py --verbose
+    HSA_NO_SCRATCH_RECLAIM=1 torchrun --nnodes 1 --nproc_per_node 4 SplitTest.py --verbose
+}
+
+# RCCL
+export TEST_BACKEND=rcclx
+run_tests
