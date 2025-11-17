1f58d8c1f174cfc02119c09c9defbdedc9b413e4
Liu Ke <liuke@meta.com>
2025-11-17 15:28:38 -0800

    implement DDA ReduceScatter
    Summary: Implement DDA ReduceScatter
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D85123680
    
    fbshipit-source-id: 24e20cf4dc48b6e6cf11d504204b3d8ab73f59a6

--- comms/common/algorithms/all_reduce/all_reduce_dda.cuh
+++ comms/common/algorithms/all_reduce/all_reduce_dda.cuh
@@ -4,43 +4,9 @@
 #include <cuda_bf16.h>
 #include <cuda_fp16.h>
 #include "comms/common/IpcGpuBarrier.cuh"
+#include "comms/common/algorithms/VecElementAdd.cuh"
 
 namespace meta::comms {
-
-template <typename T>
-concept SupportedTypes =
-    (std::same_as<T, half> || std::same_as<T, __nv_bfloat16>);
-
-template <SupportedTypes T>
-static inline __device__ uint32_t
-vecElementAdd(const uint32_t& a, const uint32_t& b) {
-  if constexpr (std::is_same<T, half>::value) {
-    const __half* x = reinterpret_cast<const __half*>(&a);
-    const __half* y = reinterpret_cast<const __half*>(&b);
-    __half2 p = __halves2half2(x[0], x[1]);
-    __half2 q = __halves2half2(y[0], y[1]);
-    __half2 z = __hadd2(p, q);
-    return (reinterpret_cast<uint32_t*>(&z))[0];
-  } else if constexpr (std::is_same<T, __nv_bfloat16>::value) {
-    const __nv_bfloat16* x = reinterpret_cast<const __nv_bfloat16*>(&a);
-    const __nv_bfloat16* y = reinterpret_cast<const __nv_bfloat16*>(&b);
-    __nv_bfloat162 p = {x[0], x[1]};
-    __nv_bfloat162 q = {y[0], y[1]};
-    __nv_bfloat162 z = __hadd2(p, q);
-    return (reinterpret_cast<uint32_t*>(&z))[0];
-  }
-  return 0;
-}
-
-template <SupportedTypes T>
-static inline __device__ uint4 vecElementAdd(const uint4& a, const uint4& b) {
-  uint4 res{0, 0, 0, 0};
-  res.x = vecElementAdd<T>(a.x, b.x);
-  res.y = vecElementAdd<T>(a.y, b.y);
-  res.z = vecElementAdd<T>(a.z, b.z);
-  res.w = vecElementAdd<T>(a.w, b.w);
-  return res;
-}
 
 template <typename T, int NRANKS, bool hasAcc>
 #if defined(USE_ROCM)
