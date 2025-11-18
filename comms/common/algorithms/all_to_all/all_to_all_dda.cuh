6ed088be8b704a5111823ea14d8bfedfeb8a721f
Liu Ke <liuke@meta.com>
2025-11-17 17:48:21 -0800

    implement DDA AllToAll
    Summary: Implement DDA AllToAll
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D85134731
    
    fbshipit-source-id: 78a887571241b746ee93acbfcb626620b97cfba8

---
+++ comms/common/algorithms/all_to_all/all_to_all_dda.cuh
@@ -0,0 +1,66 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include <cuda.h>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include "comms/common/IpcGpuBarrier.cuh"
+
+namespace meta::comms {
+
+template <typename T, int NRANKS, bool hasAcc>
+#if defined(USE_ROCM)
+__launch_bounds__(512)
+#endif
+    __global__ void ddaAllToAllIpc(
+        T* const* __restrict__ ipcbuffs,
+        T* __restrict__ recvbuff,
+        size_t count,
+        const T* __restrict__ sendbuff,
+        int selfRank,
+        IpcGpuBarrier barrier,
+        const T* __restrict__ acc) {
+  barrier.syncOnSameBlockIdx<
+      false /* hasPreviousMemAccess */,
+      true /* hasSubsequentMemAccess */>();
+
+  // use uint4 to do 16-byte loads to maximize memory efficiency
+  // We assume that count % countPerThread == 0. This assumption is enforced
+  // before kernel launch
+  // TODO: we should be able to deal with left over as well
+  constexpr auto countPerThread = sizeof(uint4) / sizeof(T);
+  const auto idxStride = gridDim.x * blockDim.x * countPerThread;
+  const auto gtIdx = blockDim.x * blockIdx.x + threadIdx.x;
+  const auto idxStart = gtIdx * countPerThread;
+  const size_t countPerRank = count;
+
+  for (size_t idx = idxStart; idx < countPerRank; idx += idxStride) {
+#pragma unroll NRANKS
+    for (int r = 0; r < NRANKS; ++r) {
+      int sendIdx = idx + r * countPerRank;
+      int destRank = r;
+      int destIdx = idx + selfRank * countPerRank;
+      *reinterpret_cast<uint4*>(&ipcbuffs[destRank][destIdx]) =
+          reinterpret_cast<const uint4*>(&sendbuff[sendIdx])[0];
+    }
+  }
+
+  barrier.syncOnSameBlockIdx<
+      true /* hasPreviousMemAccess */,
+      true /* hasSubsequentMemAccess */>();
+
+  for (size_t idx = idxStart; idx < countPerRank; idx += idxStride) {
+#pragma unroll NRANKS
+    for (int r = 0; r < NRANKS; ++r) {
+      int recvIdx = idx + r * countPerRank;
+      *reinterpret_cast<uint4*>(&recvbuff[recvIdx]) =
+          reinterpret_cast<const uint4*>(&ipcbuffs[selfRank][recvIdx])[0];
+    }
+  }
+
+  // barrier to ensure remote ranks won't free their buffers until I'm done
+  barrier.syncOnSameBlockIdx<
+      true /* hasPreviousMemAccess */,
+      false /* hasSubsequentMemAccess */>();
+}
+
+} // namespace meta::comms
---
+++ comms/common/algorithms/all_to_all/tests/all_to_all_dda_test.cu
@@ -0,0 +1,189 @@
+#include <cuda.h>
+#include <cuda_bf16.h>
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+#include <folly/init/Init.h>
+#include <folly/logging/xlog.h>
+#include <gtest/gtest.h>
+#include "comms/common/IpcGpuBarrier.cuh"
+#include "comms/common/algorithms/all_to_all/all_to_all_dda.cuh"
+#include "comms/common/tests/TestBaselineBootstrap.h"
+#include "comms/rcclx/develop/meta/lib/tests/RcclxTestUtils.h"
+#include "comms/utils/CudaRAII.h"
+
+using namespace meta::rcclx;
+using namespace meta::comms;
+
+namespace {
+constexpr int NUMRANKS = 8;
+const int cnt = 1024 * 1024;
+const int nBlocks = 32;
+const int nThreads = 128;
+} // namespace
+
+template <typename ElementType>
+class AllToAllDdaTest : public RcclxBaseTestFixture {
+ public:
+  void SetUp() override {
+    RcclxBaseTestFixture::SetUp();
+
+    ncclConfig_t config = NCCL_CONFIG_INITIALIZER;
+    NCCL_CHECK(
+        ncclCommInitRankConfig(&comm, numRanks, commId, globalRank, &config));
+    XLOGF(INFO, "rank {} init done; total ranks: {}", globalRank, numRanks);
+
+    ASSERT_EQ(numRanks, NUMRANKS);
+    CUDA_CHECK(cudaSetDevice(globalRank));
+
+    auto bootstrap = std::make_shared<TestBaselineBootstrap>(comm);
+    memHandler =
+        std::make_unique<IpcMemHandler>(bootstrap, globalRank, NUMRANKS);
+    ipcBuf =
+        std::make_unique<DeviceBuffer>(sizeof(ElementType) * NUMRANKS * cnt);
+    memHandler->addSelfDeviceMemPtr(ipcBuf->get());
+    memHandler->exchangeMemPtrs();
+
+    void* ipcBufs[NUMRANKS];
+    for (int i = 0; i < NUMRANKS; ++i) {
+      ipcBufs[i] = memHandler->getPeerDeviceMemPtr(i);
+    }
+    allRankIpcBufs =
+        std::make_unique<DeviceBuffer>(sizeof(ElementType*) * NUMRANKS);
+    CUDA_CHECK(cudaMemcpy(
+        allRankIpcBufs->get(),
+        ipcBufs,
+        sizeof(ElementType*) * NUMRANKS,
+        cudaMemcpyHostToDevice));
+
+    auto barrierInit =
+        IpcGpuBarrier::mallocAndInit(numRanks, nBlocks, globalRank, bootstrap);
+    barrierResources = std::move(barrierInit.first);
+    barrier = std::move(barrierInit.second);
+  }
+
+  void TearDown() override {
+    ncclCommFinalize(comm);
+    ncclCommDestroy(comm);
+    RcclxBaseTestFixture::TearDown();
+  }
+
+ public:
+  ncclComm_t comm{nullptr};
+  std::unique_ptr<DeviceBuffer> ipcBuf;
+  std::unique_ptr<DeviceBuffer> allRankIpcBufs;
+  std::unique_ptr<IpcMemHandler> memHandler;
+  std::unique_ptr<IpcGpuBarrierResources> barrierResources;
+  IpcGpuBarrier barrier;
+};
+
+TYPED_TEST_SUITE_P(AllToAllDdaTest);
+
+// The range of the elements in the data arrary
+const int RAND_RANGE = 100;
+
+__global__ void initRand(curandState_t* randStates, int size, int nRanks) {
+  for (auto idx = blockIdx.x * blockDim.x + threadIdx.x; idx < size;
+       idx += gridDim.x * blockDim.x) {
+    for (int i = 0; i < nRanks; i++) {
+      // The init param of curandState is same for a given randState idx on any
+      // ranks, so it will generate the same sequence of random numbers. This
+      // allows us to compute the ground truth of allReduce locally without peer
+      // rank communication.
+      curand_init(
+          i + 1 /* seed */,
+          idx /* sequence */,
+          0 /* offset */,
+          &randStates[idx * nRanks + i]);
+    }
+  }
+}
+
+template <typename T>
+__global__ void genData(
+    curandState_t* randStates,
+    T* data,
+    T* acc,
+    T* groundTruth,
+    int selfRank,
+    int nRanks,
+    int size) {
+  for (auto idx = blockIdx.x * blockDim.x + threadIdx.x; idx < size;
+       idx += gridDim.x * blockDim.x) {
+    // double val = curand_uniform_double(&randStates[idx]) * RAND_RANGE;
+    double val = 1 * RAND_RANGE;
+    for (int i = 0; i < nRanks; ++i) {
+      T hval = val;
+      if (i == selfRank) {
+        data[idx] = hval;
+      }
+      groundTruth[idx] = hval;
+    }
+  }
+}
+
+TYPED_TEST_P(AllToAllDdaTest, ddaAllToAllIpcTest) {
+  using ElementType = TypeParam;
+
+  // The IpcGpuBarrier requires numThreads >= numRanks
+  ASSERT_GE(nThreads, NUMRANKS);
+
+  // we do 128 bit load in the dda kernel, so the data memory must align with
+  // 128 bits
+  ASSERT_EQ(cnt * sizeof(ElementType) % sizeof(uint4), 0);
+
+  // prepare the sendbuff on each rank
+  DeviceBuffer randStateBuf(sizeof(curandState_t) * NUMRANKS * cnt);
+  curandState_t* randStates_d =
+      reinterpret_cast<curandState_t*>(randStateBuf.get());
+  initRand<<<nBlocks, nThreads>>>(randStates_d, cnt, NUMRANKS);
+
+  DeviceBuffer sendbuf(sizeof(ElementType) * NUMRANKS * cnt);
+  ElementType* sendbuf_d = reinterpret_cast<ElementType*>(sendbuf.get());
+
+  DeviceBuffer accbuf(sizeof(ElementType) * NUMRANKS * cnt);
+  ElementType* accbuf_d = reinterpret_cast<ElementType*>(accbuf.get());
+
+  DeviceBuffer groundTruth(sizeof(ElementType) * NUMRANKS * cnt);
+  ElementType* groundTruth_d =
+      reinterpret_cast<ElementType*>(groundTruth.get());
+
+  genData<<<nBlocks, nThreads>>>(
+      randStates_d,
+      sendbuf_d,
+      accbuf_d,
+      groundTruth_d,
+      this->globalRank,
+      NUMRANKS,
+      NUMRANKS * cnt);
+
+  DeviceBuffer recvbuff(sizeof(ElementType) * NUMRANKS * cnt);
+  ElementType* recvbuff_d = reinterpret_cast<ElementType*>(recvbuff.get());
+  ddaAllToAllIpc<ElementType, NUMRANKS, false /*hasAcc*/>
+      <<<nBlocks, nThreads>>>(
+          (ElementType**)this->allRankIpcBufs->get(),
+          recvbuff_d,
+          cnt,
+          sendbuf_d,
+          this->globalRank,
+          this->barrier,
+          accbuf_d);
+
+  cudaDeviceSynchronize();
+
+  for (int i = 0; i < NUMRANKS * cnt; ++i) {
+    EXPECT_EQ(
+        static_cast<double>(recvbuff_d[i]),
+        static_cast<double>(groundTruth_d[i]));
+  }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(AllToAllDdaTest, ddaAllToAllIpcTest);
+using TypesToTest = ::testing::Types<half, __nv_bfloat16>;
+INSTANTIATE_TYPED_TEST_SUITE_P(AllToAllDdaTests, AllToAllDdaTest, TypesToTest);
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::AddGlobalTestEnvironment(new MPIEnvironmentBase);
+  folly::Init init(&argc, &argv);
+  return RUN_ALL_TESTS();
+}
