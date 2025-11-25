eaef13a8b89169fdd58c6839e5283c9dc9fd7a99
Qiye Tan <qiyetan@meta.com>
2025-11-25 12:57:07 -0800

    move IbvPd to a separate lib
    Summary: As title
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D87852223
    
    fbshipit-source-id: 81126085574698f0c84922307f66888d3697a12b

---
+++ comms/ctran/ibverbx/IbvPd.h
@@ -0,0 +1,66 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <folly/Expected.h>
+#include "comms/ctran/ibverbx/IbvCommon.h"
+#include "comms/ctran/ibverbx/IbvMr.h"
+#include "comms/ctran/ibverbx/IbvQp.h"
+#include "comms/ctran/ibverbx/IbvVirtualQp.h"
+#include "comms/ctran/ibverbx/Ibvcore.h"
+
+namespace ibverbx {
+
+class IbvVirtualCq;
+
+// IbvPd: Protection Domain
+class IbvPd {
+ public:
+  ~IbvPd();
+
+  // disable copy constructor
+  IbvPd(const IbvPd&) = delete;
+  IbvPd& operator=(const IbvPd&) = delete;
+
+  // move constructor
+  IbvPd(IbvPd&& other) noexcept;
+  IbvPd& operator=(IbvPd&& other) noexcept;
+
+  ibv_pd* pd() const;
+  bool useDataDirect() const;
+
+  folly::Expected<IbvMr, Error>
+  regMr(void* addr, size_t length, ibv_access_flags access) const;
+
+  folly::Expected<IbvMr, Error> regDmabufMr(
+      uint64_t offset,
+      size_t length,
+      uint64_t iova,
+      int fd,
+      ibv_access_flags access) const;
+
+  folly::Expected<IbvQp, Error> createQp(ibv_qp_init_attr* initAttr) const;
+
+  // The send_cq and recv_cq fields in initAttr are ignored.
+  // Instead, initAttr.send_cq and initAttr.recv_cq will be set to the physical
+  // CQs contained within sendCq and recvCq, respectively.
+  folly::Expected<IbvVirtualQp, Error> createVirtualQp(
+      int totalQps,
+      ibv_qp_init_attr* initAttr,
+      IbvVirtualCq* sendCq,
+      IbvVirtualCq* recvCq,
+      int maxMsgCntPerQp = kIbMaxMsgCntPerQp,
+      int maxMsgSize = kIbMaxMsgSizeByte,
+      LoadBalancingScheme loadBalancingScheme =
+          LoadBalancingScheme::SPRAY) const;
+
+ private:
+  friend class IbvDevice;
+
+  IbvPd(ibv_pd* pd, bool dataDirect = false);
+
+  ibv_pd* pd_{nullptr};
+  bool dataDirect_{false}; // Relevant only to mlx5
+};
+
+} // namespace ibverbx
