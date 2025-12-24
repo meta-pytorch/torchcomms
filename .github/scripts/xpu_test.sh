d88a27d679d5b170898d1c8fba0eaaa2029c188c
Wang, Chuanqi <chuanqi.wang@intel.com>
2025-12-24 07:56:49 +0100

    [CI] Add initial CI test for XCCL backend

---
+++ .github/scripts/xpu_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+conda create -yn xpu_torchcomms_ci python=3.10
+source activate xpu_torchcomms_ci
+
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export SCCACHE_DISABLE=1
+
+export INTEL_ONEAPI=/opt/intel/oneapi
+source $INTEL_ONEAPI/compiler/latest/env/vars.sh
+source $INTEL_ONEAPI/ccl/latest/env/vars.sh
+
+export USE_XCCL=ON
+export USE_NCCL=OFF
+export USE_NCCLX=OFF
+export USE_TRANSPORT=OFF
+
+python3 -m pip install torch torchvision torchaudio pytorch-triton-xpu --index-url https://download.pytorch.org/whl/nightly/xpu --force-reinstall --no-cache-dir 
+cd torchcomms && pip install . --no-build-isolation && cd ..
+
+python3 -c "import torch; import torchcomms; print(f'Torch version: {torch.__version__}')"
+
