b6d37a7dc245ac0a7e920b8987da13770bc2f238
Pavan Balaji <pavanbalaji@meta.com>
2026-01-21 12:48:05 -0800

    Add default case to switch statements (#321)
    Summary:
    
    Add default case with TORCH_INTERNAL_ASSERT to switch statements for
    exhaustive enum handling. This satisfies compiler warnings while
    catching unexpected enum values at runtime.
    
    Reviewed By: ahmd-k
    
    Differential Revision: D91014290

--- comms/torchcomms/rcclx/TorchCommRCCLXUtils.cpp
+++ comms/torchcomms/rcclx/TorchCommRCCLXUtils.cpp
@@ -180,6 +180,8 @@ void TorchCommRCCLX::garbageCollectWorkQueues() {
           // Continue to the next element in the queue
           continue;
         }
+        default:
+          TORCH_INTERNAL_ASSERT(false, "Unexpected WorkStatus enum value");
       }
     }
 
