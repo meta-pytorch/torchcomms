cae6db023b060edd59d77b6879a8128679a53053
Pavan Balaji <pavanbalaji@meta.com>
2026-01-22 02:34:00 -0800

    Use C++17 nested namespace syntax in header files
    Summary:
    Converted TorchComms header files to use C++17 nested namespace syntax
    (namespace torch::comms) instead of the older nested style
    (namespace torch { namespace comms {). This improves consistency with
    modern C++ style and other parts of the codebase.
    
    Differential Revision: D91021910

--- comms/torchcomms/TorchCommBackend.hpp
+++ comms/torchcomms/TorchCommBackend.hpp
@@ -14,8 +14,7 @@
 #include <memory>
 #include <vector>
 
-namespace torch {
-namespace comms {
+namespace torch::comms {
 
 inline constexpr const char* TORCHCOMM_BACKEND_ABI_VERSION = "1.0";
 
@@ -182,5 +181,4 @@ struct DynamicLoaderInterface {
 // Factory function signature (implemented in each .so)
 using CreateDynamicLoaderFn = DynamicLoaderInterface (*)();
 
-} // namespace comms
-} // namespace torch
+} // namespace torch::comms
