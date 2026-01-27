2fe572190b0030f0ed439b9883abded314bbaa2e
Suhit Atul Kodgule <skodgule@meta.com>
2026-01-27 15:28:50 -0800

    Fix failing test: TestOptionsEnvironmentVariables
    Summary: The test was failing even before all changes in this diff stack. Creatd separate diff for the fix.
    
    Differential Revision: D91631437

--- comms/torchcomms/ncclx/tests/unit/cpp/TorchCommNCCLXTest.cpp
+++ comms/torchcomms/ncclx/tests/unit/cpp/TorchCommNCCLXTest.cpp
@@ -32,10 +32,10 @@ TEST_F(TorchCommNCCLXTest, TestOptionsEnvironmentVariables) {
   EXPECT_EQ(options1.abort_process_on_timeout_or_error, false);
   EXPECT_EQ(options1.timeout, std::chrono::milliseconds(1000));
 
-  setOptionsEnvironmentVariables(true, 1.5); // true abort
+  setOptionsEnvironmentVariables(true, 2.0); // true abort
   CommOptions options2;
   EXPECT_EQ(options2.abort_process_on_timeout_or_error, true);
-  EXPECT_EQ(options2.timeout, std::chrono::milliseconds(1500));
+  EXPECT_EQ(options2.timeout, std::chrono::milliseconds(2000));
 }
 
 TEST_F(TorchCommNCCLXTest, UniqueCommDesc) {
