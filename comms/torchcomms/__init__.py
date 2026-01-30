28d4e2bb2ed0189f17e5efc065759df607ed04e0
dolpm <34420038+dolpm@users.noreply.github.com>
2026-01-30 01:05:42 -0800

    Update base for Update on "fullgraph compiler integration -- coalesced collectives"
    [ghstack-poisoned]

--- comms/torchcomms/__init__.py
+++ comms/torchcomms/__init__.py
@@ -32,6 +32,10 @@ if torchcomms_compile_support_enabled:
     # to support opaque registration for time delta.
     class Timeout(timedelta, metaclass=OpaqueBaseMeta):
         pass
+else:
+    # When compile support is disabled, define Timeout without the metaclass
+    class Timeout(timedelta):
+        pass
 
 
 logger = logging.getLogger(__name__)
