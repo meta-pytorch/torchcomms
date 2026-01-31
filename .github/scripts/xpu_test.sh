f0e602211fa468585a304a0e1663bf46e34ea9d0
Wang, Chuanqi <chuanqi.wang@intel.com>
2026-01-31 08:20:53 -0800

    Add conda installation

--- .github/scripts/xpu_test.sh
+++ .github/scripts/xpu_test.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-conda create -yn xpu_torchcomms_ci python=3.10
+apt update -y && apt install -y build-essential cmake
+
+# Install Condaforge
+BASE_URL="https://github.com/conda-forge/miniforge/releases/latest/download"  # @lint-ignore
+CONDA_FILE="Miniforge3-Linux-$(uname -m).sh"
+pushd /tmp
+wget -q "${BASE_URL}/${CONDA_FILE}"
+bash "${CONDA_FILE}" -b -f -p "/opt/conda"
+popd
+sed -e 's|PATH="\(.*\)"|PATH="/opt/conda/bin:\1"|g' -i /etc/environment
+export PATH="/opt/conda/bin:$PATH"
+
+conda create -yn xpu_torchcomms_ci python=3.10 cmake
 source activate xpu_torchcomms_ci
 conda install conda-forge::glog=0.4.0 conda-forge::gflags conda-forge::fmt -y
 
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-export SCCACHE_DISABLE=1
+# export CC=/usr/bin/gcc
+# export CXX=/usr/bin/g++
+# export SCCACHE_DISABLE=1
 
 # Install oneAPI DLE
 ONEAPI_URL="https://registrationcenter-download.intel.com/akdlm/IRC_NAS/9065c156-58ab-41b0-bbee-9b0e229ffca5/intel-deep-learning-essentials-2025.3.1.15_offline.sh"
