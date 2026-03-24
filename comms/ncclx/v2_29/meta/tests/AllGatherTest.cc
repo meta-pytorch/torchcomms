6a4872a869db728cb280ecdc1ebcd153509528e0
Min Si <msi@meta.com>
2026-03-24 15:38:41 -0700

    Fix AllGatherTest to use AlgoRAII instead of EnvRAII (#1248)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/1248
    
    - Replace EnvRAII(NCCL_ALLGATHER_ALGO, algo) with AlgoRAII(NCCL_ALLGATHER_ALGO, algo) in AllGatherTest.cc across v2_27/v2_28/v2_29
    - EnvRAII only sets the environment variable, but ncclAllGather now reads the algorithm via ncclx::algoconf::getAllGatherAlgo() (runtime config), so EnvRAII has no effect and all ctran tests silently fall back to baseline NCCL
    - This caused CtranAuto_ncclMemAllocDisjoint_graph to hit an illegal memory access (baseline NCCL + disjoint memory + cuda graph is a known-buggy combination), which then caused a timeout killing the entire test binary
    - Add test_algo_utils_lib dep to allgather_test BUCK targets
    
    Reviewed By: Regina8023
    
    Differential Revision: D97796136
    
    fbshipit-source-id: eca6c8ee474e9ef3d885976e4b9994704bef3d0d

--- comms/ncclx/v2_29/meta/tests/AllGatherTest.cc
+++ comms/ncclx/v2_29/meta/tests/AllGatherTest.cc
@@ -10,10 +10,13 @@
 #include "comms/ctran/Ctran.h"
 #include "comms/ctran/algos/AllGather/AllGatherImpl.h"
 #include "comms/ncclx/meta/tests/NcclCommUtils.h"
+#include "comms/testinfra/AlgoTestUtils.h"
 #include "comms/testinfra/TestUtils.h"
 #include "comms/testinfra/TestsCuUtils.h"
 #include "comms/testinfra/TestsDistUtils.h"
 #include "comms/utils/cvars/nccl_cvars.h"
+
+using testinfra::AlgoRAII;
 
 class AllGatherTest : public NcclxBaseTest {
  public:
@@ -40,7 +43,7 @@ class AllGatherTest : public NcclxBaseTest {
       bool useCudaGraph,
       MemAllocType memType,
       size_t count) {
-    auto envGuard = EnvRAII(NCCL_ALLGATHER_ALGO, algo);
+    auto algoGuard = AlgoRAII(NCCL_ALLGATHER_ALGO, algo);
 
     if ((memType == kMemNcclMemAlloc || memType == kCuMemAllocDisjoint) &&
         ncclIsCuMemSupported() == false) {
