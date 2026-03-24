b2efd638bee818e9b5bc06cc088de7fd19ee7a4e
Tristan Rice <tristanr@meta.com>
2026-03-24 15:30:29 -0700

    Back out "Split _comms into _backend_wrapper and _store_manager" (#1229)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/1229
    
    Revert D94889998
    
    We need to revert this to do a stable release of torchcomms with PyTorch 2.11.
    
    This reverts the split of `_comms` into `_backend_wrapper` and
    `_store_manager` pybind11 modules introduced in D94889998.
    
    The BackendWrapper and StoreManager bindings are moved back into
    TorchCommPy.cpp (_comms), the separate .cpp/.pyi files and BUCK
    targets are removed, and Python imports are restored to use
    `torchcomms._comms`.
    
    Reviewed By: kapilsh, pavanbalaji
    
    Differential Revision: D97954956
    
    fbshipit-source-id: e66f190808c5be0d9f0715364ff5d02c7b6baf23

--- comms/torchcomms/TorchCommPy.cpp
+++ comms/torchcomms/TorchCommPy.cpp
@@ -8,6 +8,7 @@
 #include <torch/csrc/distributed/c10d/Store.hpp> // @manual=//caffe2:torch-cpp-cpu
 #include <torch/csrc/utils/pybind.h>
 
+#include "comms/torchcomms/BackendWrapper.hpp"
 #include "comms/torchcomms/TorchComm.hpp"
 #include "comms/torchcomms/TorchWork.hpp"
 
@@ -2056,6 +2057,56 @@ Note:
     operation is in progress.
           )doc",
           py::arg("callback"));
+
+  intrusive_ptr_class_<BackendWrapper, c10d::Backend>(m, "_BackendWrapper")
+      .def(
+          py::init<std::shared_ptr<TorchComm>>(),
+          "Create BackendWrapper around a TorchCommBackend",
+          py::arg("comm"),
+          py::call_guard<py::gil_scoped_release>())
+      .def(
+          "get_comm",
+          &BackendWrapper::getComm,
+          "Get the underlying TorchComm instance",
+          py::call_guard<py::gil_scoped_release>())
+      .def("name", &BackendWrapper::getBackendName)
+      .def_property_readonly(
+          "options",
+          &BackendWrapper::getOptions,
+          R"(Return the options used to create the torchComm under the hood.)",
+          py::call_guard<py::gil_scoped_release>())
+      .def(
+          "_verify_work_timeout",
+          &BackendWrapper::verifyWorkTimeoutForTest,
+          R"(
+Verify that a work object has the expected timeout.
+Used for testing timeout propagation.
+
+Args:
+    work: The work object to verify.
+    timeout: The expected timeout.
+
+Returns:
+    bool: True if the work object has the expected timeout, False otherwise.
+          )",
+          py::arg("work"),
+          py::arg("timeout"),
+          py::call_guard<py::gil_scoped_release>())
+      .def(
+          "_set_default_timeout",
+          &BackendWrapper::setTimeout,
+          R"(
+Set the default timeout for this backend.
+
+Args:
+    timeout: The timeout value to set.
+          )",
+          py::arg("timeout"),
+          py::call_guard<py::gil_scoped_release>());
+  intrusive_ptr_class_<WorkWrapper, c10d::Work>(m, "WorkWrapper");
+  // Register the backend Options
+  intrusive_ptr_class_<BackendWrapper::Options, c10d::Backend::Options>(
+      m, "_BackendWrapperOptions");
 
   m.def(
       "get_mem_allocator",
