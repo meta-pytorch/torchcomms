0160258c532387f70b59599175dadf6ad9ee800c
Tristan Rice <tristanr@meta.com>
2026-03-24 08:55:55 -0700

    Back out "Split _comms into _backend_wrapper and _store_manager" (#1229)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/1229
    
    Revert D94889998
    
    We need to revert this to do a stable release of torchcomms with PyTorch 2.11.
    
    This reverts the split of `_comms` into `_backend_wrapper` and
    `_store_manager` pybind11 modules introduced in D94889998.
    
    The BackendWrapper and StoreManager bindings are moved back into
    TorchCommPy.cpp (_comms), the separate .cpp/.pyi files and BUCK
    targets are removed, and Python imports are restored to use
    `torchcomms._comms`.
    
    Reviewed By: kapilsh
    
    Differential Revision: D97954956

--- comms/torchcomms/tests/unit/py/test_copy.py
+++ comms/torchcomms/tests/unit/py/test_copy.py
@@ -6,7 +6,8 @@ import os
 import unittest
 
 import torch
-from torchcomms import new_comm, ReduceOp
+from torchcomms import new_comm
+from torchcomms._comms import ReduceOp
 from torchcomms.tests.integration.py.TorchCommTestHelpers import TorchCommTestWrapper
 
 
