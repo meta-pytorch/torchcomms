697dccaf9c5e57a0aa3302f890b992c32d127036
Suhit Atul Kodgule <skodgule@meta.com>
2026-03-30 15:57:44 -0700

    Refactor Thrift Service (#264)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/264
    
    NA
    
    Reviewed By: YulunW
    
    Differential Revision: D90413413

--- comms/ncclx/v2_27/meta/commDump.cc
+++ comms/ncclx/v2_27/meta/commDump.cc
@@ -213,9 +213,9 @@ std::unordered_map<std::string, std::string> commDumpByMonitorInfo(
     XLOGF(DBG2, "CommDump: MAPPERTRACE is disabled. No trace to dump");
   }
 
-  map["ncclTopoInfo"] =
+  map["commsTopoInfo"] =
       apache::thrift::SimpleJSONSerializer::serialize<std::string>(
-          info.topoInfo.toThrift());
+          info.topoInfo);
 
   dumpProcessGlobalErrors(map);
   return map;
@@ -266,9 +266,9 @@ __attribute__((visibility("default"))) ncclResult_t ncclCommDump(
       XLOGF(DBG2, "CommDump: MAPPERTRACE is disabled. No trace to dump");
     }
 
-    map["ncclTopoInfo"] =
+    map["commsTopoInfo"] =
         apache::thrift::SimpleJSONSerializer::serialize<std::string>(
-            ncclx::comms_monitor::NcclTopoInfo::fromNcclComm(comm).toThrift());
+            ncclx::comms_monitor::getTopoInfoFromNcclComm(comm));
   }
   dumpProcessGlobalErrors(map);
 
