d91314bd875d87514530a2fe9038f7c4f0138a41
Yulun Wang <yulunwang@meta.com>
2026-04-09 16:06:48 -0700

    Add dist wrapper for reduce_scatter_quantized (#2020)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/2020
    
    Add `reduce_scatter_quantized` wrapper to `torchcomms.distwrap`.
    
    This exposes the NCCLX `reduce_scatter_quantized` backend functionality through the standard `distwrap` distributed programming interface.
    
    Reviewed By: pavanbalaji
    
    Differential Revision: D93015895
    
    fbshipit-source-id: 0765007fed3b909cc3138431b6c7f0bf1fb7ac71

--- comms/torchcomms/distwrap/__init__.py
+++ comms/torchcomms/distwrap/__init__.py
@@ -56,6 +56,7 @@ from torchcomms.distwrap.collectives_extension import (
     alltoallv_dynamic_combine,
     alltoallv_dynamic_dispatch,
     new_window,
+    reduce_scatter_quantized,
 )
 from torchcomms.distwrap.new_comm import (
     destroy_process_group,
@@ -169,6 +170,7 @@ __all__ = [
     "alltoallv_dedup_exec",
     "alltoallv_dynamic_dispatch",
     "alltoallv_dynamic_combine",
+    "reduce_scatter_quantized",
     # Re-exported from torch.distributed
     "ReduceOp",
     "P2POp",
