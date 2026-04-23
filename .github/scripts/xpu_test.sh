464fd6c1ac94c9d3c53d3b6823fb18b1b0ae8889
sramakintel <srikanth.ramakrishna@intel.com>
2026-04-23 22:38:05 +0000

    dry-run

--- .github/scripts/xpu_test.sh
+++ .github/scripts/xpu_test.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 set -ex
-# Install oneAPI DLE
-ONEAPI_URL="https://registrationcenter-download.intel.com/akdlm/IRC_NAS/b3e6c1bf-a6d5-4580-8b1d-80cbfd38c8af/intel-deep-learning-essentials-2025.3.2.36_offline.sh"
-wget -qO /tmp/intel-deep-learning-essentials.sh ${ONEAPI_URL}
-chmod +x /tmp/intel-deep-learning-essentials.sh
-/tmp/intel-deep-learning-essentials.sh -a --silent --eula accept
-rm -f /tmp/intel-deep-learning-essentials.sh
 
 export ZE_FLAT_DEVICE_HIERARCHY=FLAT
 export CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=0
 
-#source OneAPI components
-export INTEL_ONEAPI=/opt/intel/oneapi
-export TCM_ROOT=${INTEL_ONEAPI}/tcm/latest
+#source Deep Learning Essentials components
+export TCM_ROOT=${DLE_PATH}/tcm/latest
 export LD_LIBRARY_PATH="${TCM_ROOT}/lib":${LD_LIBRARY_PATH}
 
-source $INTEL_ONEAPI/umf/latest/env/vars.sh
-source $INTEL_ONEAPI/compiler/latest/env/vars.sh
-source $INTEL_ONEAPI/tbb/latest/env/vars.sh
-source $INTEL_ONEAPI/ccl/latest/env/vars.sh
-source $INTEL_ONEAPI/pti/latest/env/vars.sh
-source $INTEL_ONEAPI/mkl/latest/env/vars.sh
+source ${DLE_PATH}/umf/latest/env/vars.sh
+source ${DLE_PATH}/compiler/latest/env/vars.sh
+source ${DLE_PATH}/tbb/latest/env/vars.sh
+source ${DLE_PATH}/ccl/latest/env/vars.sh
+source ${DLE_PATH}/pti/latest/env/vars.sh
+source ${DLE_PATH}/mkl/latest/env/vars.sh
 
 #Create Conda Env and install dependencies
-conda create -yn xpu_torchcomms_ci python=3.10
-source activate xpu_torchcomms_ci
+conda create -yn xpu_torchcomms_ci-${RUNNER_NAME} python=3.10
+source activate xpu_torchcomms_ci-${RUNNER_NAME}
 conda install conda-forge::glog=0.4.0 conda-forge::gflags conda-forge::fmt -y
 
 export USE_XCCL=ON
@@ -37,7 +30,9 @@ export USE_SYSTEM_LIBS=1
 
 python3 -m pip install typing-extensions numpy sympy
 python3 -m pip install --no-deps --pre torch pytorch-triton-xpu --index-url https://download.pytorch.org/whl/nightly/xpu --force-reinstall --no-cache-dir 
-cd torchcomms && pip install . --no-deps --no-build-isolation && cd ..
+
+pip wheel ./torchcomms --no-deps --no-build-isolation --wheel-dir build-${RUNNER_NAME} && pip install --no-deps build-${RUNNER_NAME}/*.whl
+rm -rf build-${RUNNER_NAME}
 
 #Check Intel XPU visibility
 #Expose ZE_AFFINITY_MASK to explicitly expose the number of Intel GPUs assigned to the runner for all tests.
