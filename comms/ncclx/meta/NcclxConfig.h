e27eff922574859c78410e86e0d0b435aa774ced
Meenal Gupta <meenalg@meta.com>
2026-05-19 13:53:25 -0700

    Revert D105604044: Extend ncclx::Config with per-comm algo and flag fields
    Differential Revision:
    D105604044
    
    Original commit changeset: c93818960aa5
    
    Original Phabricator Diff: D105604044
    
    fbshipit-source-id: c0f650ab3458e959aea2056b74bc1c4a28e5a789

--- comms/ncclx/meta/NcclxConfig.h
+++ comms/ncclx/meta/NcclxConfig.h
@@ -7,7 +7,6 @@
 #include <string>
 #include <vector>
 
-#include "comms/utils/cvars/nccl_cvars.h"
 #include "nccl.h" // @manual
 
 namespace ncclx {
@@ -29,16 +28,6 @@ class Config {
   std::vector<int> splitGroupRanks;
   std::string ncclAllGatherAlgo = "undefined";
   bool fastInitMode = false;
-
-  bool useCtran = false;
-  bool usePatAvg = false;
-  bool noLocal = false;
-
-  enum NCCL_SENDRECV_ALGO sendrecvAlgo = NCCL_SENDRECV_ALGO::orig;
-  enum NCCL_ALLGATHER_ALGO allgatherAlgo = NCCL_ALLGATHER_ALGO::orig;
-  enum NCCL_ALLREDUCE_ALGO allreduceAlgo = NCCL_ALLREDUCE_ALGO::orig;
-  enum NCCL_ALLTOALLV_ALGO alltoallvAlgo = NCCL_ALLTOALLV_ALGO::orig;
-  enum NCCL_RMA_ALGO rmaAlgo = NCCL_RMA_ALGO::orig;
 
   // Per-communicator MultiPeerTransport (pipes) NVL config overrides.
   // When set, override the corresponding CVARs for this communicator.
@@ -63,15 +52,16 @@ class Config {
 // Hints::set() to warn on unrecognized keys (typo detection).
 inline const std::vector<std::string>& knownHintKeys() {
   static const std::vector<std::string> keys = {
-      "commDesc",          "splitGroupRanks",
-      "ncclAllGatherAlgo", "fastInitMode",
-      "useCtran",          "usePatAvg",
-      "noLocal",           "sendrecvAlgo",
-      "allgatherAlgo",     "allreduceAlgo",
-      "alltoallvAlgo",     "rmaAlgo",
-      "pipesNvlChunkSize", "pipesUseDualStateBuffer",
-      "vCliqueSize",       "ncclBuffSize",
-      "ibSplitDataOnQps",  "ibQpsPerConnection",
+      "commDesc",
+      "splitGroupRanks",
+      "ncclAllGatherAlgo",
+      "fastInitMode",
+      "pipesNvlChunkSize",
+      "pipesUseDualStateBuffer",
+      "vCliqueSize",
+      "ncclBuffSize",
+      "ibSplitDataOnQps",
+      "ibQpsPerConnection",
       "ibLazyConnect",
   };
   return keys;
