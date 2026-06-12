cd200d67f1de5430285a95b92f3fac9efc200ad4
Zhiyong Wang <zhiyongww@meta.com>
2026-06-12 12:35:50 -0700

    Add Pipes IBRC mode selection (#2691)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/2691
    
    Introduce the process-wide `NCCL_CTRAN_PIPES_IB_MODE` enum cvar with `ibgda` as the default and `ibrc_proxy` as the explicit opt-in. Wire the resolved mode through `MultiPeerTransportConfig` and fail clearly if `IBRC_PROXY` is selected before the backend is implemented.
    
    Reviewed By: snarayankh
    
    Differential Revision: D106133118
    
    fbshipit-source-id: f3ef7c537da4a633fccb2b57c26930a9e9b057a5

---
+++ comms/prims/transport/IbTransportConfig.h
@@ -0,0 +1,12 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+namespace comms::prims {
+
+enum class IbBackendMode {
+  kIbgda,
+  kIbrc,
+};
+
+} // namespace comms::prims
