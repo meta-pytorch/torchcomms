09a4aa2f36286ddba8691314878994fd54531724
sramakintel <srikanth.ramakrishna@intel.com>
2026-06-25 18:53:02 +0000

    disable full_graph_compile_Test

--- comms/torchcomms/scripts/run_tests_integration_xccl_py.sh
+++ comms/torchcomms/scripts/run_tests_integration_xccl_py.sh
@@ -34,7 +34,7 @@ run_tests () {
         FinalizeWarningTest.py
         FSDPCommTest.py
         FullgraphCompileAutogradTest.py
-        FullgraphCompileTest.py
+#        FullgraphCompileTest.py  # Disable until resolution tracked in https://jira.devtools.intel.com/browse/PYTORCHDGQ-9447
         GatherTest.py
         MemPoolTest.py
         MultiCommTest.py
