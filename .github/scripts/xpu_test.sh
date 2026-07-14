41f39f5856eae5a0a25e21c976dcec7593a57318
sramakintel <srikanth.ramakrishna@intel.com>
2026-07-14 22:40:28 +0000

    modify conda activate

--- .github/scripts/xpu_test.sh
+++ .github/scripts/xpu_test.sh
@@ -10,7 +10,8 @@ source ${DLE_PATH}/setvars.sh
 
 #Create Conda Env and install dependencies
 conda create -yn xpu_torchcomms_ci-${RUNNER_NAME} python=3.10
-source activate xpu_torchcomms_ci-${RUNNER_NAME}
+source /opt/conda/etc/profile.d/conda.sh
+conda activate xpu_torchcomms_ci-${RUNNER_NAME}
 conda install conda-forge::glog=0.4.0 conda-forge::gflags=2.2.2 conda-forge::fmt=12.2.0 -y
 
 export USE_XCCL=ON
