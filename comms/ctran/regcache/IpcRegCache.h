d301bf43637180653902d9636625aacf0a942c39
Mahidhar Ramesh Rajala <mahi@meta.com>
2026-08-12 11:13:32 -0700

    Migrate CTRAN IPC regcache errors (#3592)
    Summary:
    
    Migrate the six legacy error logs in CTRAN IPC registration and export handling to `CTRAN_ERR`. Preserve error codes, messages, arguments, Scuba reporting, return values, and control flow.
    
    Reviewed By: shr
    
    Differential Revision: D115554686

--- comms/ctran/regcache/IpcRegCache.h
+++ comms/ctran/regcache/IpcRegCache.h
@@ -18,6 +18,7 @@
 #include "comms/ctran/bootstrap/AsyncSocket.h"
 #include "comms/ctran/regcache/IpcRegCacheBase.h"
 #include "comms/ctran/utils/Checks.h"
+#include "comms/ctran/utils/CtranLogUtils.h"
 
 namespace ctran {
 
@@ -198,7 +199,7 @@ class IpcRegCache {
       ctran::regcache::IpcDesc& ipcDesc,
       std::vector<ctran::utils::CtranIpcSegDesc>& extraSegments) {
     if (ipcRegElem == nullptr) {
-      CERR(
+      CTRAN_ERR(
           commInvalidArgument,
           "CTRAN-REGCACHE: ipcRegElem is nullptr in exportMem");
       return commInvalidArgument;
