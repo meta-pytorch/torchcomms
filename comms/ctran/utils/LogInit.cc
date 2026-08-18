7bdaaeb8a3306a421e85bcb4359b557821e94e8e
Mahidhar Ramesh Rajala <mahi@meta.com>
2026-08-18 14:15:00 -0700

    Remove CTRAN Folly logger setup
    Summary:
    CTRAN production log calls now use the spdlog facade, but `initCtranLogging()` still registered three obsolete Folly handlers for CTRAN source, header, and HIP categories.
    
    Remove those production registrations and their path-specific category constants while preserving shared comms logging, spdlog configuration, last-error callbacks, level mapping, and async policy. Keep raw `CLOGF` coverage explicitly test-scoped by registering the exact test category and deriving its capture formatter from that category.
    
    Differential Revision: D116352104

--- comms/ctran/utils/LogInit.cc
+++ comms/ctran/utils/LogInit.cc
@@ -9,27 +9,11 @@
 #include "comms/ctran/utils/CtranLogger.h"
 #include "comms/utils/cvars/nccl_cvars.h" // @manual=fbcode//comms/utils/cvars:ncclx-cvars
 #include "comms/utils/logger/LogUtils.h"
-#include "comms/utils/logger/Logger.h"
 #include "comms/utils/logger/LoggingFormat.h"
 
 namespace ctran::logging {
 
 namespace {
-
-/*
- * On AMD, after hipification, the file path has format:
- * buck-out/v2/gen/fbcode/{hash}/comms/ctran/dir1/dir2/__{ori_file_name}_hipify_gen__/out/{ori_file_name},
- * The XLOG stripBuckV2Prefix https://fburl.com/code/utkyckpn doesn't handle the
- * hipified file path, so we do our own work to find the correct ctran component
- * prefix here.
- */
-std::string getHipCtranCategory() {
-  std::string fullFilePath = XLOG_FILENAME;
-  static constexpr std::string_view kCtranComponent{"comms/ctran"};
-  auto ctranComponentIdx = fullFilePath.find(kCtranComponent);
-  return fullFilePath.substr(0, ctranComponentIdx + kCtranComponent.size());
-};
-
 spdlog::level::level_enum loggerLevelToSpdlogLevel(
     meta::comms::logger::LogLevel level) {
   switch (level) {
@@ -58,48 +42,6 @@ folly::once_flag ctranLoggingInitOnceFlag;
 
 void initCtranLoggingImpl() {
   meta::comms::logger::initCommLogging();
-  NcclLogger::init(
-      NcclLoggerInitConfig{
-          .contextName = std::string{kCtranCategory},
-          .logPrefix = "CTRAN",
-          .logFilePath =
-              meta::comms::logger::parseDebugFile(NCCL_DEBUG_FILE.c_str()),
-          .logLevel = meta::comms::logger::loggerLevelToFollyLogLevel(
-              meta::comms::logger::getLoggerDebugLevel(NCCL_DEBUG)),
-          .threadContextFn = []() {
-            int cudaDev = -1;
-            (void)cudaGetDevice(&cudaDev);
-            return cudaDev;
-          }});
-  // Init logging for CTRAN header
-  NcclLogger::init(
-      NcclLoggerInitConfig{
-          .contextName = std::string{kCtranHeaderCategory},
-          .logPrefix = "CTRAN",
-          .logFilePath =
-              meta::comms::logger::parseDebugFile(NCCL_DEBUG_FILE.c_str()),
-          .logLevel = meta::comms::logger::loggerLevelToFollyLogLevel(
-              meta::comms::logger::getLoggerDebugLevel(NCCL_DEBUG)),
-          .threadContextFn = []() {
-            int cudaDev = -1;
-            (void)cudaGetDevice(&cudaDev);
-            return cudaDev;
-          }});
-#if defined(USE_ROCM)
-  NcclLogger::init(
-      NcclLoggerInitConfig{
-          .contextName = getHipCtranCategory(),
-          .logPrefix = "CTRAN",
-          .logFilePath =
-              meta::comms::logger::parseDebugFile(NCCL_DEBUG_FILE.c_str()),
-          .logLevel = meta::comms::logger::loggerLevelToFollyLogLevel(
-              meta::comms::logger::getLoggerDebugLevel(NCCL_DEBUG)),
-          .threadContextFn = []() {
-            int cudaDev = -1;
-            (void)cudaGetDevice(&cudaDev);
-            return cudaDev;
-          }});
-#endif
   meta::comms::logger::configureSpdlogLogger(
       kCtranLoggerName,
       "CTRAN",
